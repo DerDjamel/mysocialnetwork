f619426855427dd6917bcad0d7c822b2edff2e23
Dermache Djamel <dj.der.13@gmail.com>
2018-10-12 19:15:36 +0100

    adding views

---
+++ private/views/basic/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <title>SocialNetwork</title>
+        // css file
+    </head>
+    
+    <body>
+        <div class="container">
+        
+        
+    
