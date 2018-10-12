437c078525f22c8975386af8391986c739d2cf83
Dermache Djamel <dj.der.13@gmail.com>
2018-10-12 21:51:30 +0100

    minor change getting the user model

--- private/controllers/Login.php
+++ private/controllers/Login.php
@@ -5,7 +5,7 @@ class Login extends Controller {
 
     public function index(){
         // get the models here 
-        
+        //$userModel = $this->getModel('User');
         // check if user is already logged in
         if(isset($_SESSION['user_id'])){
             $user_id = filter_var($_SESSION['user_id'], FILTER_VALIDATE_INT);
